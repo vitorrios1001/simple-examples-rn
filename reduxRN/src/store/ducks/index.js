d1800ed62c5a1bb5ff5f412a7e1a52c3bd8bdf77
Vitor Rios <vitor.neto@br.experian.com>
2020-06-17 23:33:54 -0300

    Creating duck user

--- reduxRN/src/store/ducks/index.js
+++ reduxRN/src/store/ducks/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import counterReducer from './counter'
+import userReducer from './user'
 
 export default combineReducers({
-  counter: counterReducer
+  counter: counterReducer,
+  user: userReducer,
 })
